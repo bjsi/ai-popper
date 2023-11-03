c7a6613fe52f5a4ead4bd679aed44592328e19a6
Jamesb <experimentallearning0@gmail.com>
2023-11-03 18:00:33 +0000

    fix install script

--- packages/shared-lib/scripts/install.sh
+++ packages/shared-lib/scripts/install.sh
@@ -5,6 +5,7 @@ platform=$(uname -s | tr '[:upper:]' '[:lower:]')
 base_url="https://github.com/asg017/sqlite-vss/releases/download/v0.1.2"
 
 vss_extension_filename="vss0.dylib"
+vector_extension_filename="vector0.dylib"
 
 if [ "$arch" == "arm64" ] && [ "$platform" == "darwin" ]; then
   vector_extension_url="${base_url}/sqlite-vss-v0.1.2-deno-darwin-aarch64.vector0.dylib"
@@ -16,19 +17,20 @@ elif [ "$arch" == "x86_64" ] && [ "$platform" == "linux" ]; then
   vector_extension_url="${base_url}/sqlite-vss-v0.1.2-deno-linux-x86_64.vector0.so"
   vss_extension_url="${base_url}/sqlite-vss-v0.1.2-deno-linux-x86_64.vss0.so"
   vss_extension_filename="vss0.dylib.so"
+  vector_extension_filename="vector0.dylib.so"
 else
   echo "Unsupported architecture or platform: $arch on $platform"
   exit 1
 fi
 
 echo "Downloading vector extension"
-wget -O "src/lib/vector0.dylib" $vector_extension_url
-echo "Downloaded vector extension to" $(pwd) "/src/lib/vector0.dylib"
+wget -O "src/lib/"$vector_extension_filename $vector_extension_url
+echo "Downloaded vector extension"
 
 echo "Downloading vss extension"
 wget -O "src/lib/"$vss_extension_filename $vss_extension_url
-echo "Downloaded vss extension to" $(pwd) $vss_extension_filename
+echo "Downloaded vss extension"
 
 echo "Downloading vectors.sqlite"
 wget -O "src/vectors.sqlite" --no-check-certificate --no-proxy "https://ai-popper-vectors.s3.eu-north-1.amazonaws.com/vectors.sqlite"
-echo "Downloaded vectors.sqlite to" $(pwd) "/src/vectors.sqlite"
+echo "Downloaded vectors.sqlite"
